e001b0b9f2b9b32aeb084bc9d269e815d1ea2f1d
Ziyaad <xavierbaatjes@yahoo.com>
2023-07-24 19:59:48 +0200

    Update data_proc.py

--- src/data_proc.py
+++ src/data_proc.py
@@ -7,9 +7,9 @@ from params import num_samples
 
 class DataProc(torch.utils.data.Dataset):
 
-    def __init__(self, args, split):
+    def __init__(self, args):
         self.args = args
-        self.data_dict = pickle.load(open('%s/data_%s.pickle'%(args.dataset, split),'rb'))
+        self.data_dict = pickle.load(open('%s/%s.pickle'%(args.dataset, args.model_name),'rb'))
 
     def __len__(self):
         total_len = 0
@@ -20,18 +20,13 @@ class DataProc(torch.utils.data.Dataset):
 
     def __getitem__(self, item):
         rslt = []
-        n_spkrs = len(self.data_dict.keys())
-
-        for i in range(0, n_spkrs):
-            # chose random item based on prop distribution (length of each sample)
+        for i in range(0,len(self.data_dict.keys())):
+            # chose random item based on prop distribution (lenght of each sample)
             tmp_lens = [j.shape[1] for j in self.data_dict[i]]
             item = np.random.choice(len(tmp_lens),p=tmp_lens/np.sum(tmp_lens))
             rslt.append(self.random_sample(i,item))
 
-        # prepares a random sample per speaker
-        samples = {}
-        for i in range(0, n_spkrs): samples[i] = np.array(rslt)[i,:]
-        return samples
+        return {'A': np.array(rslt)[0,:], 'B': np.array(rslt)[1,:]}
 
     def augment(self,data,sample_rate=16000, pitch_shift=0.5):
         if pitch_shift == 0 : return data
